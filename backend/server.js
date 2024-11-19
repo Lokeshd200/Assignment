c158c9031204e9962048d7433d54a18fb3237232
LOKESH MELLAKANTI <143256558+Lokeshd200@users.noreply.github.com>
2024-11-20 00:08:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
